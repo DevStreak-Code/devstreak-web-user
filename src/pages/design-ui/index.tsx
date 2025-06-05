dda66bbccb7751642543c4d4d3ba3786369f5374
Sachin Gupta <sachin.gupta@peason.com>
2025-06-05 11:05:05 +0530

    Switch Button Variant Component

--- src/pages/design-ui/index.tsx
+++ src/pages/design-ui/index.tsx
@@ -1,10 +1,12 @@
 import ButtonVariants from "./ButtonVariants";
 import InputVariants from "./InputVariants";
+import SwitchButtonVariants from "./SwitchButtonVariants";
 import TextAreaVariants from "./TextAreaVariants";
 
 const DesignUI = () => {
   return (
     <div className="w-full overflow-auto flex flex-col  justify-center gap-10">
+      <SwitchButtonVariants />
       <TextAreaVariants />
       <ButtonVariants />
       <InputVariants />
