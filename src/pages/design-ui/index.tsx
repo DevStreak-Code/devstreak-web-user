9d46c8ab255eb36a57b07817cbb315a9f37c7761
Shivam Kumar <sk744826744@gmail.com>
2025-06-05 16:39:46 +0530

    Added Radio Button

--- src/pages/design-ui/index.tsx
+++ src/pages/design-ui/index.tsx
@@ -2,10 +2,12 @@ import ButtonVariants from "./ButtonVariants";
 import InputVariants from "./InputVariants";
 import SwitchButtonVariants from "./SwitchButtonVariants";
 import TextAreaVariants from "./TextAreaVariants";
+import RadioVariants from "./RadioVariants";
 
 const DesignUI = () => {
   return (
     <div className="w-full overflow-auto flex flex-col  justify-center gap-10">
+      <RadioVariants />
       <SwitchButtonVariants />
       <TextAreaVariants />
       <ButtonVariants />
