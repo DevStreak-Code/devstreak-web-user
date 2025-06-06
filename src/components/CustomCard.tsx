d434b23a5495c954313b6ddf73581b51f7c8c0cb
Aryan <aryan.gupta158935@gmail.com>
2025-06-06 23:07:17 +0530

    Update | made some changes in props

--- src/components/CustomCard.tsx
+++ src/components/CustomCard.tsx
@@ -1,14 +1,14 @@
 interface ICustomCardProps {
   url?: string;
-  title?: string;
-  alt?: "Paste your image here";
+  title?  : string;
+  alt?: string;
 }
 const CustomCard: React.FC<ICustomCardProps> = (props) => {
-  const { url, title, alt } = props;
+  const { url, title, alt} = props;
   return (
     <div className="w-5/10 m-1 ">
       <div className="border border-black w-full h-60 rounded-lg overflow-hidden">
-        <img className="h-full w-full" src={url} alt={alt} />
+        <img className="h-full w-full" src={url} alt={alt || "BG-Image"} />
       </div>
       <h1 className="mt-5 flex item-center text-[25px]">{title}</h1>
     </div>
