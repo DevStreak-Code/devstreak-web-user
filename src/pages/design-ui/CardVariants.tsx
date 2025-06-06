465161cf66857210ce6701d2c0a279632b83c8f8
Anamika Mishra <150469836+Anamika135@users.noreply.github.com>
2025-06-06 23:44:55 +0530

    features|Reusable Card|Updated

--- src/pages/design-ui/CardVariants.tsx
+++ src/pages/design-ui/CardVariants.tsx
@@ -6,35 +6,35 @@ const CardVariants = () => {
 
       <CustomCard
         imgUrl="https://tse1.mm.bing.net/th/id/OIP.psznP1mwFZNek7E9E7yL1QHaNK?rs=1&pid=ImgDetMain"
-        title="JK"
+        alt="JK"
       />
       <CustomCard
         imgUrl="https://th.bing.com/th/id/OIP.Da0L-8CTRMOdH0d8FBpxPAHaMf?w=186&h=314&c=7&r=0&o=5&pid=1.7"
-        title="J-HOPE"
+        alt="J-HOPE"
       />
       <CustomCard
         imgUrl="https://th.bing.com/th/id/OIP.ik5PRWbwpFoOtIKNY0yBRgHaNL?w=186&h=393&c=7&r=0&o=5&pid=1.7"
-        title="SUGA"
+        alt="SUGA"
       />
       <CustomCard
         imgUrl="https://wallpapers.com/images/hd/bts-rm-cute-glasses-nhn7ateb7zm2xj3j.jpg"
-        title="RM"
+        alt="RM"
       />
       <CustomCard
         imgUrl="https://th.bing.com/th/id/OIP.hSk9cU6U3ahRnPwjj8K2ogHaJ4?rs=1&pid=ImgDetMain"
-        title="JIN"
+        alt="JIN"
       />
       <CustomCard
         imgUrl="https://wallpapers.com/images/hd/jimin-pictures-kfuu8l7d9efut4br.jpg"
-        title="JIMIN"
+        alt="JIMIN"
       />
       <CustomCard
         imgUrl="https://wallpapercave.com/wp/wp5846960.jpg"
-        title="BTS"
+       alt="BTS"
       />
       <CustomCard
         imgUrl="https://th.bing.com/th/id/OIP.PXAl3bb0P6yS2RATTdiNwwHaLH?w=186&h=279&c=7&r=0&o=5&pid=1.7"
-        title="V"
+        alt="V"
       />
     </div>
   );
