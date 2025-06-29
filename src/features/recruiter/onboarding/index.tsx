fe64357c302e4b4fed2b1a5b002f22c5bb055eb8
Sachin Gupta <sachin.gupta@peason.com>
2025-06-29 23:26:00 +0530

    Space added

--- src/features/recruiter/onboarding/index.tsx
+++ src/features/recruiter/onboarding/index.tsx
@@ -1,6 +1,7 @@
 import { PrivateLayout } from "@/components/Layouts/PrivateLayout";
 
 const RecruiterOnboarding = () => {
+  
   return (
     <PrivateLayout isShowSidebar={false} isShowNavbarLogo>
       <h1>Recruiter Onboarding</h1>
